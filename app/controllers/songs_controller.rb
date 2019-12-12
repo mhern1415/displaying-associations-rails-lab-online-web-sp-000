7c32d841ff6ec8e182a7186b02bb48fe688094d9
Mark Hernandez <m.hern1415@gmail.com>
2019-12-12 23:44:23 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,17 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
-    @song = Song.new
   end
 
   def create
     @song = Song.new(song_params)
-    @song.artist = Artist.find_or_create_by(artist_params)
 
     if @song.save
       redirect_to @song
@@ -47,9 +47,5 @@ class SongsController < ApplicationController
 
   def song_params
     params.require(:song).permit(:title)
-  end
-
-  def artist_params
-    params.require(:artist).permit(:name)
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,14 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    artist.name
+  end
 end
+
+
+
+
+
+
+
